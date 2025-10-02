97e74c203b8cbeefe2428b071b6d7b8c2ae0d4e0
weeros <74591392+weeros@users.noreply.github.com>
2025-10-02 11:30:11 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,32 @@
-input.onButtonPressed(Button.A, function () {
-    numero += 1
-    basic.showNumber(numero)
-})
-input.onButtonPressed(Button.B, function () {
-    numero -= 1
-    basic.showNumber(numero)
-})
-let numero = 0
-numero = 0
+/**
+ * Angle de l'inclinaison gauche / droite
+ * 
+ * input.rotation(Rotation.Roll)
+ * 
+ * Angle de l'inclinaison haut / bas
+ * 
+ * input.rotation(Rotation.Pitch)
+ */
+// Angle de l'inclinaison haut / bas
+function point (x: number, y: number) {
+    led.unplot(prev_x, prev_y)
+    led.plot(x, y)
+    prev_x = x
+    prev_y = y
+}
+
+function degreToPoint(value : number) : number {
+    let position = 2 + (value * 5) / 180
+    if (position > 5) { return 5 }
+    if (position < 0) { return 0 }
+    return position
+}
+
+basic.forever( () => {
+    point(degreToPoint(input.rotation(Rotation.Roll)), degreToPoint(input.rotation(Rotation.Pitch)))
+}) 
+
+
+let prev_x = 2
+let prev_y = 2
+

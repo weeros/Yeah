5970fb964520d2f34063503f62ae2a965b8fedbd
weeros <74591392+weeros@users.noreply.github.com>
2025-10-02 11:32:25 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -14,19 +14,21 @@ function point (x: number, y: number) {
     prev_x = x
     prev_y = y
 }
-
-function degreToPoint(value : number) : number {
-    let position = 2 + (value * 5) / 180
-    if (position > 5) { return 5 }
-    if (position < 0) { return 0 }
+function degreToPoint (value: number) {
+    position = 2 + value * 5 / 180
+    if (position >= 4) {
+        return 4
+    }
+    if (position < 0) {
+        return 0
+    }
     return position
 }
-
-basic.forever( () => {
+let position = 0
+let prev_y = 0
+let prev_x = 0
+prev_x = 2
+prev_y = 2
+basic.forever(function () {
     point(degreToPoint(input.rotation(Rotation.Roll)), degreToPoint(input.rotation(Rotation.Pitch)))
-}) 
-
-
-let prev_x = 2
-let prev_y = 2
-
+})

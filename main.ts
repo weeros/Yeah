5ca06f758b15fc1d8fed5aa71741d8e207efab20
weeros <74591392+weeros@users.noreply.github.com>
2025-10-02 12:01:03 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,34 +1,39 @@
-/**
- * Angle de l'inclinaison gauche / droite
- * 
- * input.rotation(Rotation.Roll)
- * 
- * Angle de l'inclinaison haut / bas
- * 
- * input.rotation(Rotation.Pitch)
- */
-// Angle de l'inclinaison haut / bas
-function point (x: number, y: number) {
-    led.unplot(prev_x, prev_y)
-    led.plot(x, y)
-    prev_x = x
-    prev_y = y
-}
-function degreToPoint (value: number) {
-    position = 2 + value * 5 / 180
-    if (position >= 4) {
-        return 4
-    }
-    if (position < 0) {
-        return 0
-    }
-    return position
-}
-let position = 0
-let prev_y = 0
-let prev_x = 0
-prev_x = 2
-prev_y = 2
-basic.forever(function () {
-    point(degreToPoint(input.rotation(Rotation.Roll)), degreToPoint(input.rotation(Rotation.Pitch)))
+
+radio.onReceivedNumber(function (message) {
+    showMessage(message)
+
 })
+
+// TODO implement localId
+let localId = 0
+const showMessage = (message: number) => {
+    basic.showIcon(message)
+}
+const selectIcon = () => {
+    IconSelected = IconSelected + 1;
+}
+const sendIcon = () => {
+
+
+    for (let icon = 0; icon < 10; icon++) {
+        for (let canal = 0; canal < 255; canal++) {
+            radio.sendNumber(icon)
+            radio.setGroup(canal)
+
+        }
+
+    }
+}
+
+const changeCanal = () => {
+    canal = canal + 1;    
+}
+
+
+
+let IconSelected = 1
+let canal = 1
+radio.setFrequencyBand(0)
+input.onLogoEvent(TouchButtonEvent.Touched, changeCanal)
+input.onButtonPressed(Button.A, selectIcon);
+input.onButtonPressed(Button.B, sendIcon);
